6152bc2099b0f9307ffa03f839de85206643a2e8
Megan Chiu <megan.chiu.2014@gmail.com>
2025-01-12 23:07:40 -0800

    complete change div background exercise

---
+++ change-div-background/index.js
@@ -0,0 +1,4 @@
+// The background color of myDiv is green by default. Change the background color to yellow.
+
+const content = document.querySelector('#myDiv');
+content.style.background = 'yellow';

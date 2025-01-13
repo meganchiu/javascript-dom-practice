ae1f124006c068ec9f6901b7802d0fb509f62849
Megan Chiu <megan.chiu.2014@gmail.com>
2025-01-12 23:33:58 -0800

    complete second create dom element exercise

---
+++ create-dom-element-second/index.js
@@ -0,0 +1,7 @@
+// Insert an image with the source "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8LcG9R8Sp-1OxtmBVQff8IYm-_VTYdPdljw&s" 
+// into the <body>.
+
+const content = document.createElement('img');
+content.src = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8LcG9R8Sp-1OxtmBVQff8IYm-_VTYdPdljw&s';
+
+document.body.appendChild(content);

9b002f546cd803e782173ab140b3e47c202104e0
Megan Chiu <megan.chiu.2014@gmail.com>
2025-01-12 23:41:45 -0800

    complete dom list of li exercise

---
+++ create-dom-list-of-li/index.js
@@ -0,0 +1,13 @@
+// Set the listString variable with the value needed to have the following HTML as the body's innerHTML:
+{/* <ul>
+    <li>First Item</li>
+    <li>Second Item</li>
+    <li>Third Item</li>
+</ul> */}
+
+let beginning = "<ul>";
+let listString = "<li>FIrst Item</li><li>Second Item</li><li>Third Item</li>";
+let ending = "</ul>";
+
+// Do not modify after this line
+document.body.innerHTML = beginning + listString + ending;
